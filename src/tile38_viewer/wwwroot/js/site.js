8e461434a4a0b07e6201b53e037bb20f0ba53e9c
Leigh Hunt <leigh@venari.co.nz>
2019-09-03 22:31:34 +1200

    Addfing map

--- src/tile38_viewer/wwwroot/js/site.js
+++ src/tile38_viewer/wwwroot/js/site.js
@@ -1,4 +1,44 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+var porirua = [-41.135461, 174.839714]
+
+var map = L.map('map').setView(porirua, 11);
+
+var tileLayerOSM = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+// Available Thundermap themes:
+var theme = 'transport'; // Others: cycle, landscape, outdoors, transport-dark, spinal-map, pioneer, mobile-atlas, neighbourhood
+// theme = 'cycle';
+// theme = 'transport-dark';
+// theme = 'outdoors';
+// theme = 'neighbourhood';
+
+var tileLayerThunderforest = 'https://{s}.tile.thunderforest.com/' + theme + '/{z}/{x}/{y}{r}.png';
+
+var tileLayerUrl = tileLayerThunderforest;
+
+L.tileLayer(tileLayerUrl, {
+  opacity: 0.3,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+map.locate({setView: false, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    // L.marker(e.latlng).addTo(map)
+
+    L.circle(e.latlng, radius).addTo(map)
+          .bindPopup("You are within " + radius + " meters from this point");
+
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+map.on('locationfound', onLocationFound);
